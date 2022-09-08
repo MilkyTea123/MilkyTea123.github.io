8e2013961b103e88a21074f0af3336ede0a9506d
MilkyTea123 <63531427+MilkyTea123@users.noreply.github.com>
2022-09-08 16:38:54 -0700

    Updates

---
+++ precache-manifest.0996909041785d1d1326e435578558a4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552586995dc147576bd47df21add61bb",
+    "url": "/igemeditor/index.html"
+  },
+  {
+    "revision": "5dd0ac11c56906e05211",
+    "url": "/igemeditor/static/css/main.css"
+  },
+  {
+    "revision": "5dd0ac11c56906e05211",
+    "url": "/igemeditor/static/js/main.js"
+  },
+  {
+    "revision": "47a6bb091aa459578b95cd90574064e9",
+    "url": "/igemeditor/static/js/main.js.LICENSE.txt"
+  }
+]);
