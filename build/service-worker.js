8da627eb8de5b5edf50bb1cedfc474fcae791baa
MilkyTea123 <63531427+MilkyTea123@users.noreply.github.com>
2022-09-08 16:42:38 -0700

    deploy-changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MilkyTea123.github.io/precache-manifest.180de35a967b5d873257bddb519ec69a.js"
+  "/igemeditor/precache-manifest.0996909041785d1d1326e435578558a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MilkyTea123.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/igemeditor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
