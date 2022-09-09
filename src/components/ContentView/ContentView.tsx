cee30d5c76564d4069238806cfb65390be0e3960
MilkyTea123 <63531427+MilkyTea123@users.noreply.github.com>
2022-09-09 00:04:15 -0700

    consolelog

--- src/components/ContentView/ContentView.tsx
+++ src/components/ContentView/ContentView.tsx
@@ -31,6 +31,7 @@ export const ContentView: React.FC<ContentViewProps> = ({ contentData, pageTitle
 		}
 	}, [contentData, content]);
 
+	console.log(pageTitle);
 	let pageString = pageTitle === "" ? "MAIN_PAGE_DO_NOT_EDIT" :
 		pageTitle.substring(1, pageTitle.length);
 
